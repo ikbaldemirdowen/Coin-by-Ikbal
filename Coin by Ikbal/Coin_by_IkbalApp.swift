0b25241e0294611810b699355db43d5afa849106
Ikbal Demirdoven <ikbaldemirdoven@icloud.com>
2023-05-15 22:07:41 -0700

    Initial Commit

---
+++ Coin by Ikbal/Coin_by_IkbalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coin_by_IkbalApp.swift
+//  Coin by Ikbal
+//
+//  Created by Ikbal Demirdoven on 2023-05-15.
+//
+
+import SwiftUI
+
+@main
+struct Coin_by_IkbalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
